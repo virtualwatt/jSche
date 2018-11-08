43804d3c71c2ba2cee230bf82b7225cf2d58bc82
vtkachenko <vtkachenko@ua8955.serena.com>
2018-11-08 18:10:24 +0200

    srv compilation destination fix

--- jScheSrv/auxiliary/jScheSrv.bat
+++ jScheSrv/auxiliary/jScheSrv.bat
@@ -4,15 +4,8 @@
 @echo off
 setlocal
 
-if "%JAVA_HOME%"=="" (
-	echo Install JRE 7 or JDK 7 and set JAVA_HOME
-	exit /b 1
-)
-path=%JAVA_HOME%\bin
-
 cd %~dp0..\jScheConfigs
 
-echo JAVA_HOME=%JAVA_HOME%
 cd
 echo %time%
 echo .
